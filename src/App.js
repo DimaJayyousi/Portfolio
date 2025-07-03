4d41e995cba71570b6a93731cf5724f5d4eae6b2
Dima Jayyousi <dimajayousi@gmail.com>
2025-07-04 00:04:11 +0300

    hero section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Heder from './component/Heder/heder'
+import Heder from './component/Heder/heder';
+import Hero from  './component/hero/hero';
 function App() {
   return (
     <div>
       <Heder/>
+      <Hero/>
     </div>
     
   );
---
+++ src/component/hero/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import heroim from '../Assets/hero.png'
+import './hero.css'
+const hero = () => {
+  return (
+   <section className='hero'>
+         <img src={heroim} alt=''/>
+   
+        </section>
+  )
+}
+
+export default hero
