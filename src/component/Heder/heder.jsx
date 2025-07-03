02470a00f989c3bed1618082b3c9e74fc0a4a245
Dima Jayyousi <dimajayousi@gmail.com>
2025-07-03 23:46:21 +0300

    the nav bar

---
+++ src/component/Heder/heder.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './heder.css'
+const heder = () => {
+  return (
+    <div className='navbar'>
+      <div className="main">
+        <p>Diamond Rain </p>
+      </div>
+      <div className="nav-menu">
+        <li>About Me</li>
+        <li>Experience</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Volunteering</li>
+        <li>certifications </li>
+      </div>
+    </div>
+  )
+}
+
+export default heder
